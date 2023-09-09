aed6d9589721b1c69ca285e974741c6c236d0d2a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-09-09 11:20:28 -0500

    feat(Server): Create preprocessing pipeline

--- server/core/dataset.py
+++ server/core/dataset.py
@@ -1,12 +1,14 @@
 import shutil
 import os
 
-from preprocesor import read_audio_segments, save_audio, read_audio
+import pandas as pd
+
 from utils import enumerate_recordings
+import preprocesor as prep
 
 
 def segment_audio_files():
-    print("[*] Reading path of audio recordings...")
+    print("\n[*] Reading path of audio recordings...")
     recordings = enumerate_recordings("data/raw")
     num_files = len(recordings)
     print(f"[*] Found {num_files} audio files in the raw data directory.")
@@ -15,7 +17,7 @@ def segment_audio_files():
         root, filename = os.path.split(pathname)
         root = root.replace("data/raw", "data/.temp")
         name, extension = os.path.splitext(filename)
-        recording = read_audio_segments(pathname)
+        recording = prep.read_audio_segments(pathname)
         progress = f"{index + 1}/{num_files}"
         num_segs = len(recording)
         print(f"[*] Processing file {progress}: {filename} with {num_segs} segments...")
@@ -25,7 +27,7 @@ def segment_audio_files():
 
         for i, segment in enumerate(recording):
             new_pathname = os.path.join(root, f"{name}-{i}{extension}")
-            save_audio(new_pathname, segment)
+            prep.save_audio(new_pathname, segment)
             print(f"[+] Saved segment {i + 1}/{num_segs}: {new_pathname}")
 
     print("[+] Segmentation complete.")
@@ -44,7 +46,7 @@ def split_audio_dataset():
     print(f"[*] Train files: {num_files - num_test_files}")
 
     for index, pathname in recordings:
-        recording = read_audio(pathname)
+        recording = prep.read_audio(pathname)
         root, filename = os.path.split(pathname)
         destination = "data/test" if index <= num_test_files else "data/train"
         root = root.replace("data/.temp", destination)
@@ -53,7 +55,7 @@ def split_audio_dataset():
         if not os.path.exists(root):
             os.makedirs(root)
 
-        save_audio(pathname, recording)
+        prep.save_audio(pathname, recording)
 
         if index == num_test_files:
             print("[+] Starting to move files to train set...")
@@ -62,6 +64,73 @@ def split_audio_dataset():
     shutil.rmtree("data/.temp")
 
 
+def get_audio_metadata(pathname: str):
+    root, filename = os.path.split(pathname)
+    sound = os.path.basename(root).split("-")[1]
+    pronun = None
+
+    if sound not in ["noise"]:
+        condition = filename.split("_")[3]
+        pronun = "correct" if condition == "N" else "incorrect"
+
+    return {
+        "sound": sound,
+        "pronunciation": pronun,
+    }
+
+
+def create_dataset_file(folder: str, output: str):
+    print(f"\n[*] Reading audio recordings from folder: {folder}...")
+    recordings = enumerate_recordings(folder)
+    num_files = len(recordings)
+    rows = []
+
+    print(f"[*] Starting preprocessing of {num_files} audio recordings")
+    for index, pathname in recordings:
+        print(f"[{index}/{num_files}] Preprocessing file: {pathname}")
+        metadata = get_audio_metadata(pathname)
+        recording = prep.read_audio(pathname)
+        metadata["spectrogram"] = prep.get_spectrogram(recording)
+        rows.append(metadata)
+
+    print("[*] Exporting dataset...")
+    df = pd.DataFrame(rows)
+    df.to_json(output, orient="records")
+
+    print(f"[*] Dataset exported successfully in {output}")
+    shutil.rmtree(folder)
+
+
+def apply_data_augmentation():
+    print("\n[*] Reading path of audio recordings...")
+    recordings = enumerate_recordings("data/train")
+    num_files = len(recordings)
+
+    print(f"[*] Found {num_files} audio files in the raw data directory.")
+    print("[*] Starting data augmentation of audio recordings")
+
+    for index, pathname in recordings:
+        print(f"[{index}/{num_files}] Preprocessing file: {pathname}")
+
+        signal = prep.read_audio(pathname)
+        root, filename = os.path.split(pathname)
+        name, extension = os.path.splitext(filename)
+        pathmame = os.path.join(root, f"{name}_@{extension}")
+
+        if "background-noise" not in pathmame:
+            prep.add_random_gain(signal, pathmame.replace("@", "gain"))
+
+        prep.add_white_noise(signal, pathmame.replace("@", "white-noise"))
+        prep.add_time_stretch(signal, pathmame.replace("@", "time-stretch"))
+        prep.add_pitch_scale(signal, pathmame.replace("@", "pitch-scale"))
+
+    print("[*] Data created successfully")
+
+
 if __name__ == "__main__":
     segment_audio_files()
     split_audio_dataset()
+    apply_data_augmentation()
+
+    create_dataset_file("data/test", "data/test.json")
+    create_dataset_file("data/train", "data/train.json")
--- server/core/preprocesor.py
+++ server/core/preprocesor.py
@@ -2,6 +2,7 @@ from typing import List
 import soundfile as sf
 import numpy as np
 import librosa
+import random
 
 SAMPLE_RATE = 22050  # frequency with which instants of the audio signal
 TARGET_SAMPLES = int(SAMPLE_RATE * 0.2)  # one second worth of audio
@@ -32,3 +33,32 @@ def read_audio(file) -> np.ndarray:
 
 def save_audio(filename: str, data: np.ndarray) -> None:
     sf.write(file=filename, data=data, samplerate=SAMPLE_RATE)
+
+
+def get_spectrogram(signal: np.ndarray) -> np.ndarray:
+    spectrogram = librosa.stft(signal, n_fft=N_FFT, hop_length=HOP_LENGTH)
+    spectrogram = np.abs(spectrogram.T)
+    return np.expand_dims(spectrogram, axis=2)
+
+
+def add_white_noise(signal: np.ndarray, output: str):
+    noise_percentage_factor = 0.2
+    noise = np.random.normal(0, signal.std(), signal.size)
+    augmented_signal = signal + noise * noise_percentage_factor
+    save_audio(output, augmented_signal)
+
+
+def add_time_stretch(signal: np.ndarray, output: str):
+    augmented_signal = librosa.effects.time_stretch(signal, rate=0.3)
+    save_audio(output, augmented_signal)
+
+
+def add_pitch_scale(signal: np.ndarray, output: str):
+    augmented_signal = librosa.effects.pitch_shift(y=signal, sr=SAMPLE_RATE, n_steps=2)
+    save_audio(output, augmented_signal)
+
+
+def add_random_gain(signal: np.ndarray, output: str):
+    gain_rate = random.uniform(0.1, 0.12)
+    augmented_signal = signal * gain_rate
+    save_audio(output, augmented_signal)
