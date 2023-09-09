a2a2b2c36fe1d359fadd7567307c22312805ba30
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-09-09 17:36:11 -0500

    feat(Server): Evaluate model

---
+++ server/core/evaluate.py
@@ -0,0 +1,23 @@
+from typing import Any
+import os
+
+from keras.models import load_model
+import tensorflow as tf
+
+from utils import plot_confusion_matrix
+from dataset import load_dataset_file
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+model_path = "models/phoneme_model.keras"
+model_phoneme: Any = load_model(model_path)
+
+X_test, y, classes = load_dataset_file("data/test.json")
+y_classes = classes["sounds"]
+y_test = y["sound"]
+
+y_pred = model_phoneme.predict(X_test)
+y_pred = tf.argmax(y_pred, axis=1)
+
+confusion_mtx = tf.math.confusion_matrix(y_test, y_pred)
+plot_confusion_matrix(confusion_mtx, y_classes)
