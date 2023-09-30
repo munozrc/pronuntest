03c7c2a4e93bb3dd582ad01b3b8e3553aaba6bda
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-09-30 16:30:31 -0500

    feat(App): Add sound effect pressed main menu

--- app/screens/main_menu.gd
+++ app/screens/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var scroll = $SlidingMenu
 @onready var btn_left = $SliderControls/CenterContainer/HBoxContainer/LeftButton
 @onready var btn_right = $SliderControls/CenterContainer/HBoxContainer/RightButton
+@onready var btn_sound_effect = $SliderControls/AudioStreamPlayer
 
 var current_island: int = 0
 var separation: float = 100.0
@@ -16,10 +17,12 @@ func _show_slider_controls():
 	btn_right.modulate.a = 1.0 if current_island < 1 else 0.5
 
 func _change_current_island():
+	btn_sound_effect.play()
+	_show_slider_controls()
+	
 	var tween: Tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	var pos_x = (island_width + separation) * current_island
 	tween.tween_property(scroll, "scroll_horizontal", pos_x, 0.3)
-	_show_slider_controls()
 
 func _on_left_button_pressed():
 	if current_island == 0:
@@ -29,7 +32,7 @@ func _on_left_button_pressed():
 	_change_current_island()
 
 func _on_right_button_pressed():
-	if current_island > 1:
+	if current_island == 1:
 		return
 	
 	current_island+=1
