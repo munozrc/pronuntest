af2b429f6374bc6b5ee702549bf81e0e342a9e91
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-10-19 08:48:54 -0500

    feat: Initial activity for recognition phoneme

---
+++ app/screens/activities/02/tasks/pronunciation_mechanics.gd
@@ -0,0 +1,45 @@
+extends Task
+
+@onready var predict: PredictionComponent = $PredictionComponent
+
+var record_bus_index: int
+var record_effect: AudioEffectRecord
+
+
+func _ready():
+	record_bus_index = AudioServer.get_bus_index("Record")
+	record_effect = AudioServer.get_bus_effect(record_bus_index, 0)
+	
+	$Container/RecordButton.button_down.connect(_on_record_pressed)
+	$Container/RecordButton.button_up.connect(_on_record_released)
+	
+	predict.request_completed.connect(_on_request_completed)
+
+
+func _on_record_pressed():
+	record_effect.set_recording_active(true)
+
+
+func _on_record_released():
+	record_effect.set_recording_active(false)
+	
+	var recording: AudioStreamWAV = record_effect.get_recording()
+	recording.save_to_wav($PredictionComponent.recording)
+	predict.send_audio()
+
+func _on_request_completed(_result, response_code, _headers, body):
+	if response_code != HTTPClient.RESPONSE_OK:
+		return
+	
+	var response = JSON.parse_string(body.get_string_from_utf8())
+	var phoneme = response["phoneme"]["class"]
+	var percentage = response["phoneme"]["percentage"]
+	
+	print("Phoneme = " + phoneme)
+	print("Percentage = " + str(percentage) + "%\n")
+	
+	if phoneme != "a":
+		return
+
+	$Container/Player/PlayerAnimator.play("jump")
+	$NextButton.show_button()
---
+++ app/scripts/prediction_component.gd
@@ -0,0 +1,25 @@
+class_name PredictionComponent
+extends HTTPRequest
+
+@export var recording := "user://recording.wav"
+@export var endpoint := ""
+
+
+var _url = "http://127.0.0.1:5000/api/"
+
+
+func send_audio():
+	var file = FileAccess.open(recording, FileAccess.READ)
+	var content = file.get_buffer(file.get_length())
+	file.close()
+	
+	var headers = [	"Content-Type: multipart/form-data; boundary=\"boundary\""]
+	var body = PackedByteArray()
+
+	body.append_array("--boundary\r\n".to_utf8_buffer())
+	body.append_array("Content-Disposition: form-data; name=\"recording\"; filename=\"recording.wav\"\r\n".to_utf8_buffer())
+	body.append_array("Content-Type: audio/wav\r\n\r\n".to_utf8_buffer())
+	body.append_array(content)
+	body.append_array("\r\n--boundary--\r\n".to_utf8_buffer())
+	
+	self.request_raw(_url + endpoint, headers, HTTPClient.METHOD_POST, body)
