89b60fffa898456dfc92ce6be3de94ddc7045d1d
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-10-22 02:05:48 -0500

    feat(App): Change island menu to activity

---
+++ app/components/activity_button.gd
@@ -0,0 +1,41 @@
+extends Control
+
+
+enum STATE_BUTTON { completed, unlocked, locked }
+
+
+@export var activity: PackedScene = null
+@export var state := STATE_BUTTON.locked
+
+
+@onready var buttons = {
+	"completed": $CompletedButton,
+	"unlocked": $UnlockedButton,
+	"locked": $LockedButton
+}
+
+
+func _ready():
+	self._enable_current_button()
+	buttons["completed"].pressed.connect(self._on_button_pressed)
+	buttons["unlocked"].pressed.connect(self._on_button_pressed)
+
+
+func _enable_current_button():
+	for key in buttons:
+		var button: TextureButton = buttons[key]
+		var is_active = key == STATE_BUTTON.keys()[state]
+		
+		button.visible = is_active
+		button.disabled = not is_active
+		
+		if key == "unlocked":
+			$UnlockedButton/GlobalAnimator.play("idle")
+
+
+func _on_button_pressed():
+	if activity == null:
+		printerr("Activity is required to change")
+		return
+	
+	get_tree().change_scene_to_packed(activity)
