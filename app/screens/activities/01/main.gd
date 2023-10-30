aec21aa1d589e2064e7bc70343144f77ede79f4d
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-10-30 16:45:59 -0500

    fix(App): Update path to back activity 01

--- app/screens/activities/01/main.gd
+++ app/screens/activities/01/main.gd
@@ -4,7 +4,7 @@ extends Activity
 func _start_outro_animation():
 	$TransitionComponent.play("fade_in")
 	await $TransitionComponent.animation_finished
-	get_tree().change_scene_to_file("res://screens/forest/main.tscn")
+	get_tree().change_scene_to_file("res://screens/forest.tscn")
 
 
 func _on_header_close():
