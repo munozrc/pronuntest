4dd6b06f58c2ae4052a3ff844d70d0a61755d679
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-11-06 18:17:54 -0500

    feat(App): island menu optimization

--- app/scripts/island.gd
+++ app/scripts/island.gd
@@ -7,11 +7,24 @@ signal selected
 
 @export var button: TextureButton = null
 @export var scene: PackedScene = null
+@export var visible_notifier : VisibleOnScreenNotifier2D = null
 
 
 func _ready():
 	if button != null:
 		button.pressed.connect(_on_button_pressed)
+	
+	if visible_notifier != null:
+		visible_notifier.screen_entered.connect(_on_screen_entered)
+		visible_notifier.screen_exited.connect(_on_screen_exited)
+
+
+func _on_screen_entered():
+	self.get_child(0).show()
+
+
+func _on_screen_exited():
+	self.get_child(0).hide()
 
 
 func _on_button_pressed():
