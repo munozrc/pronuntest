349ad9f8f56e1eb7adb791bebd19ff4d013de6c8
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-11-07 08:33:32 -0500

    fix(App): Fix drag box mechanics

--- app/scripts/draggable_item.gd
+++ app/scripts/draggable_item.gd
@@ -6,55 +6,40 @@ signal selected
 signal released
 
 
-@export var area: Area2D
 @export var content: String
 @export var is_active:= true
+@export var button: TouchScreenButton
 
 
-var _is_draggable := false
 var _is_selected := false
 var _origin := Vector2.ZERO
 
 
 func _ready():
-	self.area.mouse_entered.connect(self._on_mouse_entered)
-	self.area.mouse_exited.connect(self._on_mouse_exited)
+	button.pressed.connect(_on_button_pressed)
+	button.released.connect(_on_button_released)
 	self._origin = global_position
 
 
 func _physics_process(delta):
 	var mouse_pos = get_global_mouse_position()
-	var new_pos = mouse_pos if self._is_selected else self._origin
-	var speed = 25 if self._is_selected else 10
+	var new_pos = mouse_pos if _is_selected else _origin
+	var speed = 25 if _is_selected else 10
 	
 	global_position = lerp(global_position, new_pos, speed * delta)
 
 
-func _input(event):
-	if (
-		not event is InputEventScreenTouch or 
-		not self._is_draggable or 
-		not self.is_active
-	):
-		return
-	
-	if event.is_released():
-		self._is_selected = false
-		self.released.emit()
-		return
-	
-	self._is_selected = true
+func _on_button_pressed():
+	_is_selected = true
 	selected.emit()
 
 
-func _on_mouse_entered():
-	self._is_draggable = true
-
-
-func _on_mouse_exited():
-	self._is_draggable = false
+func _on_button_released():
+	_is_selected = false
+	released.emit()
 
 
 func set_origin(pos: Vector2):
-	self._is_selected = false
-	self._origin = pos
+	_is_selected = false
+	is_active = false
+	_origin = pos
