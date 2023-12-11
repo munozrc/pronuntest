07b3c70dcaa393db0d6f6d43718381d918c86d15
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-12-10 23:53:20 -0500

    feat(App): Add url api

--- app/scripts/prediction_component.gd
+++ app/scripts/prediction_component.gd
@@ -5,8 +5,9 @@ extends HTTPRequest
 @export var endpoint := ""
 
 
-#var _url = "http://192.168.0.12:4000/api/"
-var _url = "https://pronuntest-api-f5zmx.ondigitalocean.app/api/"
+var _url = "http://192.168.0.12:4000/api/"
+#var _url = "https://pronuntest-api-f5zmx.ondigitalocean.app/api/"
+#var _url = "http://146.190.3.58/api/"
 
 
 func send_audio():
